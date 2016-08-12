21ff4a2c042747520dc44281729d622d0cbe3e67
Alvipe <alvipe5@protonmail.com>
2016-08-12 12:31:03 +0200

    Refactor library to comply with Arduino style guide

--- SeCo.h
+++ SeCo.h
@@ -5,14 +5,17 @@
 
 class SeCo {
     public:
-        void receiveData(float* dataIn);
-        void receiveArray(float* dataArrayIn, unsigned int dataPoints);
-        void transmitData(float* dataOut);
-        void transmitArray(float* dataArrayOut, unsigned int dataPoints);
+        SeCo(Stream &serial);
+        void readData(float* dataIn);
+        void readArray(float* dataArrayIn, unsigned int dataPoints);
+        void writeData(float* dataOut);
+        void writeArray(float* dataArrayOut, unsigned int dataPoints);
     private:
         bool waitHeader();
         bool checkMessage(uint8_t* message, unsigned int messageSize);
         void getMessage(uint8_t* message, unsigned int messageSize);
+        Stream* _serial;
+        uint8_t check;
 };
 
 #endif
